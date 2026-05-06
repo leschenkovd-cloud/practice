875d7cf7ab01231cacb13a98e9a9cdb6e29e4996
leschenkoVD <leschenkovd@mer.ci.nsu.ru>
2026-05-06 16:15:38 +0700

    MainActivity

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
 }
